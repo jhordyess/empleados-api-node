fbb3815e38221a66bfb140ed7d6b474ff7b365ba
jhordyess <dev@jhordyess.com>
2024-01-24 17:09:42 +0000

    Removed JS files ⭐

--- src/utils/classes.js
+++
@@ -1,7 +0,0 @@
-export class HttpError extends Error {
-  constructor(message = 'SERVER ERROR', status = 500, publicError = true) {
-    super(message)
-    this.status = status
-    this.publicError = publicError
-  }
-}
